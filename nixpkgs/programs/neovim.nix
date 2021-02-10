bfbf035cb6b3663466f425786265b043dcd9878c
Nelson Jeppesen <50854675+NelsonJeppesen@users.noreply.github.com>
2021-02-10 10:33:19 -0800

    fix neovim update

--- nixpkgs/programs/neovim.nix
+++ nixpkgs/programs/neovim.nix
@@ -6,62 +6,59 @@
       enable = true;
       viAlias = true;
       vimAlias = true;
-      #defaultEditor = true;
+      vimdiffAlias = true;
 
-      configure = {
+      plugins =
+        with pkgs.vimPlugins; [
+         #neovim-sensible
+         #nvim-lspconfig
+         #vim-grammarous
+         ale
+         deoplete-nvim
+         gruvbox-community
+         vim-airline
+         vim-airline-themes
+         vim-better-whitespace
+         vim-gitgutter
+         vim-nix
+         vim-table-mode
+        ];
 
-        customRC = ''
-          " enable true color
-          set termguicolors
-          colorscheme gruvbox
-          set number
+      extraConfig = ''
+        " enable true color
+        set termguicolors
+        colorscheme gruvbox
+        set number
 
-          " autocompletion (deoplete)
-          inoremap <silent><expr> <Tab>  pumvisible() ? "<C-n>" : "<Tab>"
-          let g:deoplete#enable_at_startup = 1
+        " autocompletion (deoplete)
+        inoremap <silent><expr> <Tab>  pumvisible() ? "<C-n>" : "<Tab>"
+        let g:deoplete#enable_at_startup = 1
 
-          " copy everything to the clipboard
-          command! CopyToClipboard :%y+
-          set clipboard=unnamedplus
+        " copy everything to the clipboard
+        command! CopyToClipboard :%y+
+        set clipboard=unnamedplus
 
-          " enable mouse
-          set mouse=a
+        " enable mouse
+        set mouse=a
 
-          " Set leader:
-          map , <Leader>
-          set updatetime=70
-          let g:ale_sign_column_always = 1
+        " Set leader:
+        map , <Leader>
+        set updatetime=70
+        let g:ale_sign_column_always = 1
 
-          " Indentation settings for using 2 spaces instead of tabs.
-          set shiftwidth=2
-          set softtabstop=2
-          set expandtab
+        " Indentation settings for using 2 spaces instead of tabs.
+        set shiftwidth=2
+        set softtabstop=2
+        set expandtab
 
-          " Use case-insensitive search
-          set ignorecase
+        " Use case-insensitive search
+        set ignorecase
 
-          " cant spell
-          set spelllang=en
-          nnoremap <silent> <Leader>s :call ToggleSpellCheck()<CR>
+        " cant spell
+        set spelllang=en
+        nnoremap <silent> <Leader>s :call ToggleSpellCheck()<CR>
+
         '';
-
-        packages.myVimPackage = {
-          start = with pkgs.vimPlugins; [
-            #neovim-sensible
-            #nvim-lspconfig
-            #vim-grammarous
-            ale
-            deoplete-nvim
-            gruvbox-community
-            vim-airline
-            vim-airline-themes
-            vim-better-whitespace
-            vim-gitgutter
-            vim-nix
-            vim-table-mode
-          ] ;
-        };
-      };
     };
   };
 }
