73d82707144b64ca90ca977c2e7241a4df9eb1cd
Nelson Jeppesen <50854675+NelsonJeppesen@users.noreply.github.com>
2021-06-07 21:55:17 +0000

    use dev branch of papwerwm for gnome40 support

---
+++ nixpkgs/overlays/paperwm.nix
@@ -0,0 +1,16 @@
+
+self: super: {
+
+  gnomeExtensions = super.gnomeExtensions // {
+
+  paperwm = super.gnomeExtensions.paperwm.overrideDerivation (old: {
+    version = "pre-40.0";
+    src = builtins.fetchGit {
+      url = https://github.com/paperwm/paperwm.git;
+      ref = "next-release";
+};
+  });
+  };
+
+
+}
