33c428d8e46a3f9fdbda65d0cb2a9b892021e1ce
Nelson Jeppesen <50854675+NelsonJeppesen@users.noreply.github.com>
2021-06-23 09:05:27 -0700

    cleanup

--- nixpkgs/overlays/paperwm.nix
+++ nixpkgs/overlays/paperwm.nix
@@ -1,16 +1,11 @@
-
 self: super: {
-
   gnomeExtensions = super.gnomeExtensions // {
-
-  paperwm = super.gnomeExtensions.paperwm.overrideDerivation (old: {
-    version = "pre-40.0";
-    src = builtins.fetchGit {
-      url = https://github.com/paperwm/paperwm.git;
-      ref = "next-release";
-};
-  });
+    paperwm = super.gnomeExtensions.paperwm.overrideDerivation (old: {
+      version = "pre-40.0";
+      src = builtins.fetchGit {
+        url = https://github.com/paperwm/paperwm.git;
+        ref = "next-release";
+      };
+    });
   };
-
-
 }
