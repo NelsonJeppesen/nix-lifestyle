ccad1836829a3bbb8e1d46ee38dd5acf4677cc20
Nelson Jeppesen <50854675+NelsonJeppesen@users.noreply.github.com>
2021-12-24 21:50:24 -0800

    tidy font installs; kitty color scheme

--- nixpkgs/kitty.nix
+++ nixpkgs/kitty.nix
@@ -13,9 +13,8 @@
       enable = true;
 
       font = {
-        package = pkgs.nerdfonts;
-        #name = "VictorMono Nerd Font Mono";
-        name = "Lekton Nerd Font";
+        name    = "Lekton Nerd Font";
+        package = pkgs.nerdfonts.override { fonts = [ "Lekton"]; };
       };
 
       keybindings = {
@@ -54,27 +53,58 @@
         window_border_width         = "0.0pt";
         window_margin_width         = "7";
 
-        background            = "#2c2c2c";
-        foreground            = "#cccccc";
-        cursor                = "#cccccc";
-        selection_background  = "#505050";
-        color0                = "#000000";
-        color8                = "#000000";
-        color1                = "#f17779";
-        color9                = "#f17779";
-        color2                = "#99cc99";
-        color10               = "#99cc99";
-        color3                = "#ffcc66";
-        color11               = "#ffcc66";
-        color4                = "#6699cc";
-        color12               = "#6699cc";
-        color5                = "#cc99cc";
-        color13               = "#cc99cc";
-        color6                = "#66cccc";
-        color14               = "#66cccc";
-        color7                = "#fffefe";
-        color15               = "#fffefe";
-        selection_foreground  = "#2c2c2c";
+        # https://github.com/rebelot/kanagawa.nvim/blob/master/extras/kanagawa.conf
+        background                  = "#2E2E37";  # modified to make vim and term bg different
+
+        foreground                  = "#DCD7BA";
+        selection_background        = "#2D4F67";
+        selection_foreground        = "#C8C093";
+        url_color                   = "#72A7BC";
+        cursor                      = "#C8C093";
+        active_tab_background       = "#2D4F67";
+        active_tab_foreground       = "#DCD7BA";
+        inactive_tab_background     = "#223249";
+        inactive_tab_foreground     = "#727169";
+        color0                      = "#090618";
+        color1                      = "#C34043";
+        color2                      = "#76946A";
+        color3                      = "#C0A36E";
+        color4                      = "#7E9CD8";
+        color5                      = "#957FB8";
+        color6                      = "#6A9589";
+        color7                      = "#C8C093";
+        color8                      = "#727169";
+        color9                      = "#E82424";
+        color10                     = "#98BB6C";
+        color11                     = "#E6C384";
+        color12                     = "#7FB4CA";
+        color13                     = "#938AA9";
+        color14                     = "#7AA89F";
+        color15                     = "#DCD7BA";
+        color16                     = "#FFA066";
+        color17                     = "#FF5D62";
+
+        #background            = "#2c2c2c";
+        #foreground            = "#cccccc";
+        #cursor                = "#cccccc";
+        #selection_background  = "#505050";
+        #color0                = "#000000";
+        #color8                = "#000000";
+        #color1                = "#f17779";
+        #color9                = "#f17779";
+        #color2                = "#99cc99";
+        #color10               = "#99cc99";
+        #color3                = "#ffcc66";
+        #color11               = "#ffcc66";
+        #color4                = "#6699cc";
+        #color12               = "#6699cc";
+        #color5                = "#cc99cc";
+        #color13               = "#cc99cc";
+        #color6                = "#66cccc";
+        #color14               = "#66cccc";
+        #color7                = "#fffefe";
+        #color15               = "#fffefe";
+        #selection_foreground  = "#2c2c2c";
       };
     };
   };
