310920bba4b9eda6605f62c9f21988a024b0afa3
Nelson Jeppesen <50854675+NelsonJeppesen@users.noreply.github.com>
2022-02-24 21:45:27 -0800

    test

--- nixos/profiles/shared.nix
+++ nixos/profiles/shared.nix
@@ -8,22 +8,6 @@
   environment.defaultPackages = lib.mkDefault [];       # Remove default pacakges
   hardware.video.hidpi.enable = lib.mkDefault true;
 
-  # Clean prompt with the features I need
-  programs.zsh.enable     = lib.mkDefault true;
-  users.defaultUserShell  = pkgs.zsh;
-  programs.starship = {
-    enable = lib.mkDefault true;
-    settings = {
-      # Disabled
-      aws.disabled        = true;
-      helm.disabled       = true;
-      terraform.disabled  = true;
-
-      # Enabled
-      kubernetes.disabled = false;
-    };
-  };
-
   environment.sessionVariables = rec {
     MOZ_ENABLE_WAYLAND= "1";
   };
---
+++ nixos/profiles/zsh.nix
@@ -0,0 +1,20 @@
+{ config, pkgs, stdenv, lib, ... }:
+
+{
+  programs.zsh.enable     = true;
+
+  users.defaultUserShell  = pkgs.zsh;
+
+  programs.starship = {
+    enable = lib.mkDefault true;
+    settings = {
+      # Disabled
+      aws.disabled        = true;
+      helm.disabled       = true;
+      terraform.disabled  = true;
+
+      # Enabled
+      kubernetes.disabled = false;
+    };
+  };
+}
