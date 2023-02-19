4daf54ee68acb5c76b84e44dec022a64cc7ea3c0
Nelson Jeppesen <50854675+NelsonJeppesen@users.noreply.github.com>
2023-02-19 09:25:39 -0800

    a

---
+++ home-manager/nix-on-droid.nix
@@ -0,0 +1,60 @@
+{ config, lib, pkgs, ... }:
+
+{
+  # Simply install just the packages
+  environment.packages = with pkgs; [
+    # User-facing stuff that you really really want to have
+    #vim # or some other editor, e.g. nano or neovim
+
+    # Some common stuff that people expect to have
+    #diffutils
+    #findutils
+    #utillinux
+    #tzdata
+    #hostname
+    #man
+    #gnugrep
+    #gnupg
+    #gnused
+    #gnutar
+    #bzip2
+    #gzip
+    #xz
+    #zip
+    #unzip
+  ];
+
+  # Backup etc files instead of failing to activate generation if a file already exists in /etc
+  environment.etcBackupExtension = ".bak";
+
+  # Read the changelog before changing this value
+  system.stateVersion = "22.11";
+  terminal.font = "${pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; }}/share/fonts/truetype/NerdFonts/Fira Code Regular Nerd Font Complete Mono.ttf";
+
+  # Set up nix for flakes
+  #nix.extraOptions = ''
+  #  experimental-features = nix-command flakes
+  #'';
+
+  # Set your time zone
+  #time.timeZone = "Europe/Berlin";
+
+  # After installing home-manager channel like
+  #   nix-channel --add https://github.com/rycee/home-manager/archive/release-22.11.tar.gz home-manager
+  #   nix-channel --update
+  # you can configure home-manager in here like
+  #home-manager = {
+  #  useGlobalPkgs = true;
+  #
+  #  config =
+  #    { config, lib, pkgs, ... }:
+  #    {
+  #      # Read the changelog before changing this value
+  #      home.stateVersion = "22.11";
+  #
+  #      # insert home-manager config
+  #    };
+  #};
+}
+
+# vim: ft=nix
