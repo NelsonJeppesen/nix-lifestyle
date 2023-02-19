600c6ee68d0509c77444c9a5e60296df9f1d02af
Nelson Jeppesen <50854675+NelsonJeppesen@users.noreply.github.com>
2023-02-19 09:05:14 -0800

    a

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -17,10 +17,10 @@
 
       extraConfig = {
         # Sign all commits using ssh key
-        commit.gpgsign = true;
-        gpg.format = "ssh";
-        gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
-        user.signingkey = "~/.ssh/id_ed25519.pub";
+        #commit.gpgsign = true;
+        #gpg.format = "ssh";
+        #gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
+        #user.signingkey = "~/.ssh/id_ed25519.pub";
 
         pull = { ff = "only"; };
         push = { default = "current"; };
