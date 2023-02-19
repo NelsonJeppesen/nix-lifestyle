deb1fadd5d5bfdc11e4ad9a231e950b89cf5a53b
Nelson Jeppesen <50854675+NelsonJeppesen@users.noreply.github.com>
2023-02-19 08:56:25 -0800

    a

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,84 +1,84 @@
 { config, lib, pkgs, ... }:
 # Add `openssh` to git-sync path so it can use sshkeys to sign my commits
-let
-  git-sync = pkgs.git-sync.overrideAttrs (oldAttrs: rec {
-    wrapperPath = with lib; makeBinPath [
-      pkgs.inotify-tools
-      pkgs.coreutils
-      pkgs.git
-      pkgs.gnugrep
-      pkgs.gnused
-      pkgs.openssh
-    ];
-  });
-in
+#let
+#  git-sync = pkgs.git-sync.overrideAttrs (oldAttrs: rec {
+#    wrapperPath = with lib; makeBinPath [
+#      pkgs.inotify-tools
+#      pkgs.coreutils
+#      pkgs.git
+#      pkgs.gnugrep
+#      pkgs.gnused
+#      pkgs.openssh
+#    ];
+#  });
+#in
 {
   nixpkgs.config.allowUnfree = true;
 
-  imports = [
-    ./git.nix
-    ./gnome.nix
-    ./kitty.nix
-    ./neovim.nix
-    ./shell.nix
-  ];
+  #imports = [
+  #  ./git.nix
+  #  ./gnome.nix
+  #  ./kitty.nix
+  #  ./neovim.nix
+  #  ./shell.nix
+  #];
 
   programs.home-manager.enable = true;
 
-  programs.helix.enable = true;
-  programs.helix.settings = {
-    theme = "drucula";
-  };
+  #programs.helix.enable = true;
+  #programs.helix.settings = {
+  #  theme = "drucula";
+  #};
 
   # Add local scripts
-  home.sessionPath = [ "/home/nelson/.local/bin" ];
+  #home.sessionPath = [ "/home/nelson/.local/bin" ];
 
-  services.git-sync.enable = true;
-  services.git-sync.package = git-sync; # use patched derivation
-  services.git-sync.repositories.notes.uri = "manualy-git-clone-the-repo";
-  services.git-sync.repositories.notes.path = "/home/nelson/s/notes";
+  #services.git-sync.enable = true;
+  #services.git-sync.package = git-sync; # use patched derivation
+  #services.git-sync.repositories.notes.uri = "manualy-git-clone-the-repo";
+  #services.git-sync.repositories.notes.path = "/home/nelson/s/notes";
 
-  programs.firefox = {
-    enable = true;
-    profiles = {
-      home = {
-        id = 0;
-        name = "home";
-        userChrome = "
-          #TabsToolbar
-          { visibility: collapse; }
-        ";
-        settings = {
-          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-          "layers.acceleration.force-enabled" = true;
-          "gfx.webrender.all" = true;
-          "gfx.webrender.enabled" = true;
-          "layout.css.backdrop-filter.enabled" = true;
-          "svg.context-properties.content.enabled" = true;
-        };
-      };
-    };
-  };
+  #programs.firefox = {
+  #  enable = true;
+  #  profiles = {
+  #    home = {
+  #      id = 0;
+  #      name = "home";
+  #      userChrome = "
+  #        #TabsToolbar
+  #        { visibility: collapse; }
+  #      ";
+  #      settings = {
+  #        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+  #        "layers.acceleration.force-enabled" = true;
+  #        "gfx.webrender.all" = true;
+  #        "gfx.webrender.enabled" = true;
+  #        "layout.css.backdrop-filter.enabled" = true;
+  #        "svg.context-properties.content.enabled" = true;
+  #      };
+  #    };
+  #  };
+  #};
 
   home = {
     stateVersion = "22.05";
-    username = "nelson";
-    homeDirectory = "/home/nelson";
+    username = "nix-on-droid";
+    homeDirectory = "/data/data/com.termux.nix/files/home";
 
 
-    file.".local/bin".source = ../bin;
-    file.".terraform.d/plugin-cache/.empty".source = ../dotfiles/empty;
-    file.".config/fend/config.toml".source = ../dotfiles/fend.toml;
+    #file.".local/bin".source = ../bin;
+    #file.".terraform.d/plugin-cache/.empty".source = ../dotfiles/empty;
+    #file.".config/fend/config.toml".source = ../dotfiles/fend.toml;
 
     packages = [
-      pkgs.flameshot
+      #pkgs.flameshot
       #pkgs.ddcutil
 
       # jeppesen.io
       #pkgs.hugo
 
       # nixpkgs maintainer
-      pkgs.nixpkgs-review
+      #pkgs.nixpkgs-review
 
       # fun
       #pkgs.mindustry
@@ -92,15 +92,15 @@ in
 
       # chill
       #pkgs.somafm-cli forked
-      pkgs.spotify
+      #pkgs.spotify
 
       # core GUI apps
-      pkgs.bitwarden
-      pkgs.fractal
+      #pkgs.bitwarden
+      #pkgs.fractal
       #pkgs.google-chrome
-      pkgs.kitty
-      pkgs.slack
-      pkgs.zoom-us
+      #pkgs.kitty
+      #pkgs.slack
+      #pkgs.zoom-us
       #pkgs.libreoffice
 
       # cloud management
@@ -137,7 +137,7 @@ in
       pkgs.up
       pkgs.vault
       pkgs.wget
-      pkgs.wl-clipboard
+      #pkgs.wl-clipboard
 
       # Kubernetes
       pkgs.aws-iam-authenticator
