dd44933eac870ec18e7bb525169499bda14cb29d
Nelson Jeppesen <50854675+NelsonJeppesen@users.noreply.github.com>
2023-04-01 11:28:02 -0700

    tidy

--- home-manager/install.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p ~/.config
-ln -s $(pwd)  ~/.config/home-manager
-
-nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-nix-channel --update
-
-nix-shell '<home-manager>' -A install
