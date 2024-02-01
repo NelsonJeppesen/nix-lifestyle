ecee999ed407ff70619356c7693554dd9b6ad407
Nelson Jeppesen <50854675+NelsonJeppesen@users.noreply.github.com>
2024-01-31 16:49:23 -0800

    zram

--- nixos/machines/gram14.nix
+++ nixos/machines/gram14.nix
@@ -25,13 +25,12 @@
 
   system.stateVersion = "22.11";
 
+  # laptop has 8gb of ram and it can get tight sometimes
+  zramSwap.memoryPercent = 100;
+
   boot.initrd.luks.devices.root.device = "/dev/disk/by-uuid/c4a5062a-5060-4351-8f70-7cec63cd0487";
   fileSystems."/".device = "/dev/disk/by-uuid/87a61706-201f-469c-b399-490f83109760";
   fileSystems."/".fsType = "btrfs";
   fileSystems."/".options = [ "noatime" "nodiratime" "discard=async" "autodefrag" ];
-
   fileSystems."/boot".device = "/dev/disk/by-uuid/E7AF-1131";
-
-  #swapDevices = [{ device = "/dev/disk/by-uuid/c09b6f9b-c7ad-4672-91c2-895a40de81b5"; }];
-  #boot.kernel.sysctl = { "vm.swappiness" = 10; };
 }
