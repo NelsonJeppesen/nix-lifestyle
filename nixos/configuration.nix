15032013df639d4dbfb29c8b9ace5ab03bf925fc
Nelson Jeppesen <50854675+NelsonJeppesen@users.noreply.github.com>
2024-06-14 20:45:05 -0700

    init k3s cluster

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,22 +1,26 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 let
   # read the computer serial number to index in the hostnameSet variable
-  hostname = hostnameSet.${
-      lib.removeSuffix "\n" (builtins.readFile /sys/class/dmi/id/board_serial)
-    };
+  hostname = hostnameSet.${lib.removeSuffix "\n" (builtins.readFile /sys/class/dmi/id/board_serial)};
 
   # map serial to hostname
   hostnameSet = {
+    # laptops
     "A0D62C851DEF02CBA03BAF34FE26013F14Z90Q-K.ARW5U1" = "gram14";
     "6637E90D521FA763D230F439CB4C2C2F17Z90Q-K.AAC7U1" = "gram17";
+
+    # laptop k3s cluster
     "C02740702SAHJD41M" = "macbook12-0";
-    "C02TW48EHH21 " = "macbook12-1";
+    "C0272460DKHHJ9K1C" = "macbook12-1";
+    "C0284530316HJ9V14" = "macbook12-2";
   };
-in {
+in
+{
   networking.hostName = hostname;
   networking.domain = "local";
 
   imports = [
-    ./machines/${hostname}.nix # map hostname to nix import
+    # map host name to nix import
+    ./machines/${hostname}.nix
   ];
 }
