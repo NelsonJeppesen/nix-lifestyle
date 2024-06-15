6d6980c9864a49ce33b870b05e5b1695ee1ebc25
Nelson Jeppesen <50854675+NelsonJeppesen@users.noreply.github.com>
2024-06-15 15:50:34 +0000

    fix

--- nixos/profiles/macbook12-server.nix
+++ nixos/profiles/macbook12-server.nix
@@ -6,21 +6,21 @@
 
   networking.networkmanager.enable = true;
 
-  systemd.services.console-fbset = {
-    enable = true;
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStartPost = "${pkgs.util-linux}/bin/setterm -resize";
-      ExecStartPre = "/run/current-system/sw/bin/sleep 15";
-      ExecStart =
-        "${pkgs.fbset}/bin/fbset -fb /dev/fb0 -g 2304 1440 2304 1440 32";
-      TTYPath = "/dev/console";
-      StandardOutput = "tty";
-      StandardInput = "tty-force";
-    };
-    wantedBy = [ "multi-user.target" ];
-    environment = { TERM = "linux"; };
-  };
+  #systemd.services.console-fbset = {
+  #  enable = true;
+  #  serviceConfig = {
+  #    Type = "oneshot";
+  #    ExecStartPost = "${pkgs.util-linux}/bin/setterm -resize";
+  #    ExecStartPre = "/run/current-system/sw/bin/sleep 15";
+  #    ExecStart =
+  #      "${pkgs.fbset}/bin/fbset -fb /dev/fb0 -g 2304 1440 2304 1440 32";
+  #    TTYPath = "/dev/console";
+  #    StandardOutput = "tty";
+  #    StandardInput = "tty-force";
+  #  };
+  #  wantedBy = [ "multi-user.target" ];
+  #  environment = { TERM = "linux"; };
+  #};
 
   systemd.services.console-blank = {
     enable = true;
