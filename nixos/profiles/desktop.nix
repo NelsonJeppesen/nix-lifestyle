f71eb6060031fb4e00908e5c7f88766367143145
Nelson Jeppesen <50854675+NelsonJeppesen@users.noreply.github.com>
2024-06-23 18:34:40 -0700

    misc

--- nixos/profiles/desktop.nix
+++ nixos/profiles/desktop.nix
@@ -10,7 +10,7 @@ let
   # dumb hack to get godaddy certs working outside of a browser
   cert = builtins.fetchurl {
     url = "https://certs.godaddy.com/repository/gd_bundle-g2-g1.crt";
-    sha256 = "0aa4014e6a34e2553b6258c671946620a44e03aedf0fde497781fe8a6abfd858";
+    sha256 = "0n6qpxm8mzl1fx4xw3yzmq1lx910csa73ijqc8xmbqild970390a";
   };
 in
 {
