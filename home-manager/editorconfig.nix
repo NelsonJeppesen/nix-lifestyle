e55ce587959b44436ea16b67ff384cbaae296538
Nelson Jeppesen <50854675+NelsonJeppesen@users.noreply.github.com>
2024-06-29 21:52:39 -0700

    editorconfig move

---
+++ home-manager/editorconfig.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, ... }:
+{
+  editorconfig = {
+    enable = true;
+    settings = {
+      "*" = {
+        charset = "utf-8";
+        end_of_line = "lf";
+        trim_trailing_whitespace = true;
+        insert_final_newline = true;
+        max_line_width = 0;
+        indent_style = "space";
+        indent_size = 2;
+      };
+    };
+  };
+}
