6c953964bb84c29a1f175aaccd047dc8c1532460
Nelson Jeppesen <50854675+NelsonJeppesen@users.noreply.github.com>
2024-07-04 20:08:05 -0700

    tighten-it-up

--- home-manager/kitty.nix
+++ home-manager/kitty.nix
@@ -1,4 +1,5 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }:
+{
   home = {
     file = {
       ".config/kitty/kitty.startup.session".text = ''
@@ -62,8 +63,8 @@
         font_family = "Red Hat Mono Medium";
         font_size = "12.5";
 
-        window_border_width = "1.0pt";
-        window_margin_width = "7";
+        window_border_width = "1px";
+        window_margin_width = "0";
         active_border_color = "#74B3CE";
         inactive_border_color = "#aaaaaa";
         bell_border_color = "#ff5a00";
@@ -71,8 +72,7 @@
         # Don't use patched fonts
         # have kitty bring in Symbols from Nerd Font
         # https://sw.kovidgoyal.net/kitty/faq/#kitty-is-not-able-to-use-my-favorite-font
-        symbol_map =
-          "U+23FB-U+23FE,U+2665,U+26A1,U+2B58,U+E000-U+E00A,U+E0A0-U+E0A3,U+E0B0-U+E0D4,U+E200-U+E2A9,U+E300-U+E3E3,U+E5FA-U+E6AA,U+E700-U+E7C5,U+EA60-U+EBEB,U+F000-U+F2E0,U+F300-U+F32F,U+F400-U+F4A9,U+F500-U+F8FF,U+F0001-U+F1AF0 Symbols Nerd Font Mono";
+        symbol_map = "U+23FB-U+23FE,U+2665,U+26A1,U+2B58,U+E000-U+E00A,U+E0A0-U+E0A3,U+E0B0-U+E0D4,U+E200-U+E2A9,U+E300-U+E3E3,U+E5FA-U+E6AA,U+E700-U+E7C5,U+EA60-U+EBEB,U+F000-U+F2E0,U+F300-U+F32F,U+F400-U+F4A9,U+F500-U+F8FF,U+F0001-U+F1AF0 Symbols Nerd Font Mono";
 
         allow_remote_control = "socket-only";
         listen_on = "unix:/tmp/kitty";
@@ -85,11 +85,11 @@
         # turn off Input Method Extensions which add latency and create bugs
         wayland_enable_ime = false;
 
-        inactive_text_alpha = "0.65";
+        inactive_text_alpha = "0.50";
         linux_display_server = "wayland";
         scrollback_lines = "30000";
         shell_integration = "no-cursor";
-        strip_trailing_spaces = "smart";
+        strip_trailing_spaces = "always";
         tab_bar_min_tabs = 1;
         tab_bar_style = "powerline";
         tab_powerline_style = "slanted";
