b7cec6d81afdb2dabe6a6b5f9618e42c20d30535
Nelson Jeppesen <50854675+NelsonJeppesen@users.noreply.github.com>
2024-09-17 22:25:02 -0700

    drop my aliases :(

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -36,54 +36,6 @@
           helper = "store";
         };
       };
-
-      aliases = {
-        a = "add";
-        ap = "add * --patch";
-        br = "!git co $(git branch --list --sort=-authordate |fzf --height 15)";
-        c = "commit -m";
-        co = "checkout";
-
-        de = "!git diff --name-only | uniq | xargs vim";
-
-        d1 = "diff HEAD~1";
-        d2 = "diff HEAD~2";
-        d3 = "diff HEAD~3";
-        d4 = "diff HEAD~4";
-
-        da = "!git diff --name-only | uniq | xargs git add";
-        dap = "!git diff --name-only | uniq | xargs git add --patch";
-
-        cp = "cherry-pick";
-        cpa = "cherry-pick --abort";
-        cpc = "cherry-pick --continue";
-
-        d = "diff";
-        dfm = "diff origin/main";
-        dfmast = "diff origin/master";
-        ps = "push";
-        psf = "push --force-with-lease";
-
-        rb = "rebase --interactive origin/main";
-        rbm = "rebase --interactive origin/master";
-        rba = "rebase --abort";
-        rbc = "rebase --continue";
-        rbh = "rebase --interactive HEAD~9";
-
-        rl = "reflog";
-        s = "status";
-        some = "!git fetch -a && git pull";
-
-        st = "stash";
-        stc = "stash clear";
-        stp = "stash pop";
-
-        w = "worktree";
-        wa = "worktree add";
-        wl = "worktree list";
-        wp = "worktree prune";
-        wr = "worktree remove";
-      };
     };
   };
 }
