9e4868f346da57040e39a65eeacc55242e425bba
Nelson Jeppesen <50854675+NelsonJeppesen@users.noreply.github.com>
2025-05-05 11:14:25 -0700

    tidy

--- home-manager/neovim.nix
+++ home-manager/neovim.nix
@@ -53,16 +53,14 @@
         vim.opt.swapfile    = false
 
         -- Configure LSP/diagnostic icons
-        local icons = {
-          [vim.diagnostic.severity.ERROR] = "",
-          [vim.diagnostic.severity.WARN]  = "",
-          [vim.diagnostic.severity.INFO]  = "",
-          [vim.diagnostic.severity.HINT]  = "󰌵",
-        }
-
         vim.diagnostic.config({
           signs = {
-            text = icons,
+            text = {
+              [vim.diagnostic.severity.ERROR] = "",
+              [vim.diagnostic.severity.WARN]  = "",
+              [vim.diagnostic.severity.INFO]  = "",
+              [vim.diagnostic.severity.HINT]  = "󰌵",
+            },
           },
           virtual_text   = false,
           underline      = true,
