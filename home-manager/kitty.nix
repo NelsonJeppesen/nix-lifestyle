a00b5e820575ac9d9d789afb101fcb9dfc4bcfc1
Nelson Jeppesen <50854675+NelsonJeppesen@users.noreply.github.com>
2025-05-15 19:11:37 -0700

    custom bright

--- home-manager/kitty.nix
+++ home-manager/kitty.nix
@@ -7,12 +7,10 @@
         launch
       '';
 
-      ".config/kitty/light-theme.auto.conf".source =
-        pkgs.kitty-themes + "/share/kitty-themes/themes/Adapta_Nokto_Maia.conf";
-      ".config/kitty/no-preference-theme.auto.conf".source =
-        pkgs.kitty-themes + "/share/kitty-themes/themes/Adapta_Nokto_Maia.conf";
+      ".config/kitty/light-theme.auto.conf".source = ./dotfiles/kitty.light.conf;
+      ".config/kitty/no-preference-theme.auto.conf".source = ./dotfiles/kitty.light.conf;
       ".config/kitty/dark-theme.auto.conf".source =
-        pkgs.kitty-themes + "/share/kitty-themes/themes/GruvboxMaterialDarkHard.conf";
+        pkgs.kitty-themes + "/share/kitty-themes/themes/shadotheme.conf";
     };
   };
 
