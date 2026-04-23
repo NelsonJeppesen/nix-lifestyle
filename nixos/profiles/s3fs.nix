0ed6275f520be1c96979a6583567dd5569e41e4b
Nelson Jeppesen <50854675+NelsonJeppesen@users.noreply.github.com>
2026-04-23 15:09:21 -0700

    s3fs: lazy automount with 10-min idle timeout; add header comment

--- nixos/profiles/s3fs.nix
+++ nixos/profiles/s3fs.nix
@@ -1,3 +1,10 @@
+# s3fs.nix - Mount an AWS S3 bucket as a local filesystem via s3fs-fuse.
+#
+# - Bucket: jeppesen.io-s3fs in us-west-2
+# - Credentials: agenix-managed at /etc/secrets/encrypted/s3fs.creds.age
+# - Mounted lazily via systemd automount; idles after 10 min of inactivity
+# - Page cache lives at /var/cache/s3fs (s3fs-managed)
+# - allow_other lets non-root users access the mount (FUSE security boundary)
 { config, pkgs, ... }:
 {
   age.secrets.s3fs-creds.file = /etc/secrets/encrypted/s3fs.creds.age;
@@ -8,7 +15,14 @@
     fsType = "fuse./run/current-system/sw/bin/s3fs";
     noCheck = true;
     options = [
+      # Defer mount until network-online and don't fail boot if S3 is
+      # unreachable; mount lazily on first access via systemd automount.
       "_netdev"
+      "nofail"
+      "x-systemd.automount"
+      "x-systemd.idle-timeout=600"
+      "x-systemd.requires=network-online.target"
+      "x-systemd.after=network-online.target"
       "rw"
       "allow_other"
       "url=https://s3.us-west-2.amazonaws.com"
