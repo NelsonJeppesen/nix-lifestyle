d27b1afe7b51be10528150ab1811a17938937b1b
Nelson Jeppesen <50854675+NelsonJeppesen@users.noreply.github.com>
2026-08-01 12:04:04 -0700

    networking: add HAProxy 3.0 CA prod hosts entries

--- nixos/profiles/networking.nix
+++ nixos/profiles/networking.nix
@@ -43,4 +43,17 @@
     pkgs.networkmanager-openvpn
     pkgs.networkmanager-fortisslvpn
   ];
+
+  # HaProxy 3.0 CA Prod - hosts file test entries
+  networking.extraHosts = ''
+    # nslookup prodca-haproxy30-webs-pd-16168ebdf291eab3.elb.ca-central-1.amazonaws.com
+    16.52.53.237     privatedomain.alchemer-ca.com
+
+    # nslookup prodca-haproxy30-apps-public-1284526185.ca-central-1.elb.amazonaws.com
+    52.60.223.159    app.alchemer-ca.com reporting.alchemer-ca.com api.alchemer-ca.com
+
+    # nslookup prodca-haproxy30-webs-public-2056957719.ca-central-1.elb.amazonaws.com
+    16.52.65.105    survey.alchemer-ca.com
+    16.52.65.105    s.alchemer-ca.com
+  '';
 }
